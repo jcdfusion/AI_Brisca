1765fcd5240405042dd52bc1f656d75a79817405
jcdfusion <jcdfusion@users.noreply.github.com>
2017-04-21 16:41:58 -0400

    Update Agent.java

--- com/ai/brisca_agent/Agent.java
+++ com/ai/brisca_agent/Agent.java
@@ -18,9 +18,30 @@ public class Agent {
 
 
     }
-    public void maxValue(){
-        //TODO
-    }
+   public void maxValue(Card[] myCards, Card[] otherPlayerCards) {
+		// TODO
+		int value = fetchMaxValue(myCards);
+		int opponentValue = fetchMaxValue(otherPlayerCards);
+	}
+
+	/**
+	 * Retrieves the highest value card in a collection of Card objects.
+	 * 
+	 * @param cards
+	 *            The array of cards to search.
+	 * @return The highest value card in the collection.
+	 */
+	private int fetchMaxValue(Card[] cards) {
+		// Stores the card with the highest value.
+		int max = cards[0].getValue();
+		// Loop through all cards and replace max with the highest value card.
+		for (int i = 1; i < cards.length; i++) {
+			if (cards[i].getValue() > max) {
+				max = cards[i].getValue();
+			}
+		}
+		return max;
+	}
     public void minValue(){
         //TODO
     }
